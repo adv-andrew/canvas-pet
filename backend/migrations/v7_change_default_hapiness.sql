9f4790d163ad4e743d38ae58a3a4207db924f9d9
juancavallin <juancavallin@gmail.com>
2026-04-20 15:32:19 -0400

    typo in migration v7

--- backend/migrations/v7_change_default_hapiness.sql
+++ backend/migrations/v7_change_default_hapiness.sql
@@ -1,3 +1,3 @@
 -- Set starting hapiness to 80 so that it stars as "Ecstatic" but makes them work to keep them at that level and try to reach 100
-ALTER TABLE users
+ALTER TABLE canvas_users
 ALTER COLUMN happiness_score SET DEFAULT 80;
